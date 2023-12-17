61694c079a5702092f21207d6472d8bd16dd363a
jefcrb <jef@ssicc.be>
2023-12-17 13:01:42 +0100

    Update script to include CRS

--- caddy/main.go
+++ caddy/main.go
@@ -8,8 +8,23 @@ import (
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
 
 	_ "github.com/corazawaf/coraza-caddy/v2"
+
+	_ "github.com/corazawaf/coraza-coreruleset"
+
+	_ "github.com/jcchavezs/mergefs"
+
+	_ "github.com/jcchavezs/mergefs/io"
 )
 
 func main() {
+	waf, err := coraza.NewWAF(
+        coraza.NewWAFConfig().
+            WithDirectives(`
+                Include @owasp_crs/REQUEST-911-METHOD-ENFORCEMENT.conf
+                Include myrules/*.conf
+            `).
+            WithRootFS(mergefs.Merge(coreruleset.FS, io.OSFS)),
+    )
+
 	caddycmd.Main()
 }
